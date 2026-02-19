e5ca2aabeb410a40294ec54da4b5d2a30d4d51dd
MythreyeS <43024481+MythreyeS@users.noreply.github.com>
2026-02-19 10:25:49 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,20 +1,28 @@
-import os
 import requests
+import os
 
-def send_message(msg):
 
-    token = os.getenv("TELEGRAM_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-    if not token or not chat_id:
-        print("Telegram credentials missing")
+
+def send_telegram(message):
+
+    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        print("Telegram credentials missing.")
         return
 
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": chat_id,
-        "text": msg
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
     }
 
-    requests.post(url, data=payload)
+    response = requests.post(url, data=payload)
+
+    if response.status_code == 200:
+        print("Telegram message sent successfully.")
+    else:
+        print("Failed to send Telegram message.")
+        print(response.text)
